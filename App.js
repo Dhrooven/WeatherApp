f4f5e736522fc3e5ae2a6a39cd7783a08272d89c
Dhrooven <j.dhruv315@gmail.com>
2023-10-21 01:42:16 +0530

    Added OpenWeatherMap.org's API data setup

--- App.js
+++ App.js
@@ -8,10 +8,48 @@ import {
   PermissionsAndroid,
 } from 'react-native';
 import Geolocation from '@react-native-community/geolocation';
+import {WEATHER_API_KEY} from '.env';
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-  const [location, setLocation] = useState(null); // Initialize location state to null
+  // const [location, setLocation] = useState(null); // Initialize location state to null
+  const [weather, setWeather] = useState([]); // Initialize weather state
+  const [errorState, setErrorState] = useState(null); // Initialize error state to null
+  const [lat, setLat] = useState([]); // Initialize latitude
+  const [lon, setLon] = useState([]); // Initialize longitude
+
+  const fetchWeather = async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`,
+      );
+      const data = await res.json();
+      setWeather(data);
+    } catch (err) {
+      setErrorState('Could not fetch weather data');
+      setLoading(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+        setLat(position.coords.latitude); // Update the Latitude state with the received position
+        setLon(position.coords.longitude); // Update the Longitude state with the received position
+        setLoading(false); // Set loading to false after getting the location
+      },
+      error => {
+        console.log(error.code, error.message);
+        setLoading(true); // Set loading to false if there's an error in getting the location
+        setErrorState('Could not get location');
+      },
+      {enableHighAccuracy: false, timeout: 15000}, // keeping accuracy as false works, but if i change it to true, it breaks.
+    );
+  };
 
   useEffect(() => {
     const requestLocationPermission = async () => {
@@ -29,36 +67,23 @@ const App = () => {
         if (granted === PermissionsAndroid.RESULTS.GRANTED) {
           console.log('You can use Geolocation');
           getLocation();
+          await fetchWeather();
         } else {
           console.log('You cannot use Geolocation');
-          setLoading(false);
+          setLoading(true);
         }
       } catch (err) {
         console.warn(err);
-        setLoading(false);
+        setLoading(true);
       }
     };
     requestLocationPermission();
-  }, []);
+  }, [lat, lon]);
 
   useEffect(() => {
-    console.log(location);
-  }, [location]);
-
-  const getLocation = () => {
-    Geolocation.getCurrentPosition(
-      position => {
-        console.log(position);
-        setLocation(position); // Update the location state with the received position
-        setLoading(false); // Set loading to false after getting the location
-      },
-      error => {
-        console.log(error.code, error.message);
-        setLoading(true); // Set loading to false if there's an error in getting the location
-      },
-      {enableHighAccuracy: false, timeout: 15000},
-    );
-  };
+    console.log(lat);
+    console.log(lon);
+  }, [lat, lon]);
 
   if (loading) {
     return (
